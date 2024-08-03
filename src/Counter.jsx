002b84fef043069dea1f7ad5acb2d2656e0b04e1
nitin aggarwal <agg.nitin124@gmail.com>
2024-08-03 18:14:42 +0530

    Final Commit

---
+++ src/Counter.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count,setCount] = useState(0);
+    const handleClick  = (operation) =>{
+      setCount(operation==="+"?count+1:count-1);
+    }
+  return (
+    <div>
+        <h1>Counter App</h1>
+        <p>Count:{count}</p>
+        <button onClick={()=>handleClick("+")}>Intrement</button>
+        <button onClick={()=>handleClick("-")}>Decrement</button>
+    </div>
+  )
+}
+
+export default Counter
